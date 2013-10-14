fc793d5a9d6e7a8c59d13437759364defdb218be
joaonc <joaonc@gmail.com>
2013-10-14 03:18:54 +0000

    Started unit testing Added helper classes to deal with file masking (unfinished)

---
+++ JRenamer/JRenamer/DropDownItemList.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JRenamer
+{
+    public class DropDownItemList : List<DropDownItem>
+    {
+        public DropDownItem FindByDescription(string description)
+        {
+            return this.Find(
+                delegate(DropDownItem item)
+                {
+                    return item.Description.Equals(description);
+                }
+            );
+        }
+
+        public DropDownItem FindByMask(string mask)
+        {
+            return this.Find(
+                delegate(DropDownItem item)
+                {
+                    return item.Description.Equals(mask);
+                }
+            );
+        }
+    }
+}
---
+++ JRenamer/JRenamerUnitTest/DropDownItemTest.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using JRenamer;
+
+namespace JRenamerUnitTest
+{
+    [TestClass]
+    public class DropDownItemTest
+    {
+        private string descriptionTest = "Test Description";
+        private string maskAll = "*.*";
+
+        [TestMethod]
+        public void ToStringTest1()
+        {
+            DropDownItem item = new DropDownItem(descriptionTest, false, maskAll);
+
+            Assert.AreEqual(item.ToString(), descriptionTest);
+        }
+
+        [TestMethod]
+        public void ToStringTest2()
+        {
+            DropDownItem item = new DropDownItem(descriptionTest, true, maskAll);
+
+            Assert.AreEqual(item.ToString(), descriptionTest + " (*.*)");
+        }
+
+        [TestMethod]
+        public void ToStringTest3()
+        {
+            DropDownItem item = new DropDownItem(descriptionTest, false, "*.1", "*.2");
+
+            Assert.AreEqual(item.ToString(), descriptionTest);
+        }
+
+        [TestMethod]
+        public void ToStringTest4()
+        {
+            DropDownItem item = new DropDownItem(descriptionTest, true, "*.1", "*.2");
+
+            Assert.AreEqual(item.ToString(), descriptionTest + " (*.1 | *.2)");
+        }
+
+        [TestMethod]
+        public void GetMaskAsStringSeparatorStandard1()
+        {
+            DropDownItem item = new DropDownItem(descriptionTest, false, maskAll);
+
+            Assert.AreEqual(item.GetMaskAsString(), maskAll);
+        }
+
+        [TestMethod]
+        public void GetMaskAsStringSeparatorStandard2()
+        {
+            DropDownItem item = new DropDownItem(descriptionTest, false, "*.1", "*.2");
+
+            Assert.AreEqual(item.GetMaskAsString(), "*.1 | *.2");
+        }
+
+        [TestMethod]
+        public void GetMaskAsStringSeparatorCustom1()
+        {
+            DropDownItem item = new DropDownItem(descriptionTest, false, maskAll);
+
+            Assert.AreEqual(item.GetMaskAsString(", "), maskAll);
+        }
+
+        [TestMethod]
+        public void GetMaskAsStringSeparatorCustom2()
+        {
+            DropDownItem item = new DropDownItem(descriptionTest, false, "*.1", "*.2");
+
+            Assert.AreEqual(item.GetMaskAsString(", "), "*.1, *.2");
+        }
+
+        [TestMethod]
+        public void Description1()
+        {
+            DropDownItem item = new DropDownItem(descriptionTest, false, maskAll);
+
+            Assert.AreEqual(item.Description, descriptionTest);
+        }
+
+        [TestMethod]
+        public void Description2()
+        {
+            DropDownItem item = new DropDownItem(descriptionTest, true, maskAll);
+
+            Assert.AreEqual(item.Description, descriptionTest);
+        }
+
+        [TestMethod]
+        public void Description3()
+        {
+            DropDownItem item = new DropDownItem(descriptionTest, false, "*.1", "*.2");
+
+            Assert.AreEqual(item.Description, descriptionTest);
+        }
+
+        [TestMethod]
+        public void Description4()
+        {
+            DropDownItem item = new DropDownItem(descriptionTest, true, "*.1", "*.2");
+
+            Assert.AreEqual(item.Description, descriptionTest);
+        }
+    }
+
+}
